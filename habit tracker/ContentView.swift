2adf724dfd02b599f2b294a5abc02a8b98c164d8
Jocelyn <jocelyndbaker@gmail.com>
2024-01-02 16:25:42 +1300

    Initial Commit

---
+++ habit tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  habit tracker
+//
+//  Created by Jocelyn on 2024-01-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ habit tracker/habit_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  habit_trackerApp.swift
+//  habit tracker
+//
+//  Created by Jocelyn on 2024-01-02.
+//
+
+import SwiftUI
+
+@main
+struct habit_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
